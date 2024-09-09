60c9f3e848cd6117df60224234048a077a28629e
ozgedrk <ozge.dirik.38@hotmail.com>
2024-09-09 11:43:37 +0300

    Docker, bashscript

--- workshop.1-mssql.sql
+++ workshop.1-mssql.sql
@@ -4,4 +4,51 @@
 Multiple Comment
 */
 
-use northwind
+-- Iliskisel Veritabani (RDMS)
+-- Iliskisel Olmayan Veritabani (NoSql)
+
+/*
+- Veritabani : 
+- Organize edilmis ve yapilandirilmis veri koleksyionlarina denir.
+- Veriyi duzenli, erisilebilirligini saglayan, veri tekrarini onleyecek sekilde hazirlanmalidir.(1NF, 2NF, 3NF, 4NF, BCND)
+*/
+
+/*
+- Veritabani Bilesenleri: 
+- Veri
+- Tablolar
+- Satir(rows)
+- Sutunlar(cols)
+- Iliskiler
+- Anahtarlar (PK, FK)
+*/
+
+/*
+- Veritabani Genel Kurallari (common rules): 
+- Sutun adlarini hepsi kucuk ve yeni kelimeler arasinda (_) konulmalidir. 
+	orn: category_name 
+*/
+
+
+
+/*
+- Iliskisel Veritabanlari
+- Mysql
+- Postgresql
+- Mssql
+- Oracle
+- H2DB
+- MariaDB
+*/
+
+
+/*
+- Iliskisel Olmayan Veritabanlari
+- MongoDB
+- Cassandra
+- Redis
+- Neo4j
+*/
+
+
+use northwind ;
